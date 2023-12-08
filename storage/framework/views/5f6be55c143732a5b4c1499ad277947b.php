8d51f76410b4198e75268dcb06d7170e34e77d8e
Rubens Nogueira <rubens.jesus1997@gmail.com>
2023-12-08 11:13:40 -0300

    back-end
    back-end

---
+++ storage/framework/views/5f6be55c143732a5b4c1499ad277947b.php
@@ -0,0 +1,9 @@
+<div>
+    <h2>Contato</h2>
+   <p><b>Nome: </b> <?php echo e($nome); ?></p>  
+   <p><b>E-mail: </b> <?php echo e($email); ?></p>  
+   <p><b>Celular: </b> <?php echo e($celular); ?></p>  
+   <p><b>Empresa: </b> <?php echo e($empresa); ?></p>
+   <p><b>Mensagem: </b> <?php echo e($mensagem); ?></p>  
+</div>
+<?php /**PATH C:\Users\dev02\OneDrive\Área de Trabalho\empresa\api\resources\views/mail/pre-cadastro.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/c62b6a10a110481304168ac82d8edbe0.php
@@ -0,0 +1,8 @@
+<div>
+    <h2>Contato</h2>
+   <p><b>Nome: </b><?php echo e($nome); ?></p>  
+   <p><b>E-mail: </b><?php echo e($email); ?></p>  
+   <p><b>Celular: </b><?php echo e($celular); ?></p>  
+   <p><b>Mensagem: </b><?php echo e($mensagem); ?></p>  
+</div>
+<?php /**PATH C:\Users\dev02\OneDrive\Área de Trabalho\empresa\api\resources\views/mail/contato.blade.php ENDPATH**/ ?>
